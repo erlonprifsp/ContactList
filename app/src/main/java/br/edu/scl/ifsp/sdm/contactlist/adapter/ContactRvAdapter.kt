0afa996d42733db14416964e5243e95d987b66bd
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2023-11-24 10:35:28 -0300

    pratica - fase 3.2 - 12:07

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/adapter/ContactRvAdapter.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/adapter/ContactRvAdapter.kt
@@ -3,7 +3,9 @@ package br.edu.scl.ifsp.sdm.contactlist.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import br.edu.scl.ifsp.sdm.contactlist.R
 import br.edu.scl.ifsp.sdm.contactlist.databinding.TileContactBinding
 import br.edu.scl.ifsp.sdm.contactlist.model.Contact
 import br.edu.scl.ifsp.sdm.contactlist.view.OnContactClickListener
@@ -13,6 +15,20 @@ class ContactRvAdapter(private val contactList: MutableList<Contact>, private va
         val nameTv: TextView = tileContactBinding.nameTv
         val emailTv: TextView = tileContactBinding.emailTv
         val phoneTv: TextView = tileContactBinding.phoneTv
+
+        // executado após o construtor primário
+        init {
+            // cria para cada célula um menu de contexto
+            tileContactBinding.root.apply {
+                setOnCreateContextMenuListener { menu, _, _ ->
+                    (onContactClickListener as AppCompatActivity).menuInflater.inflate(R.menu.context_menu_main, menu)
+                    menu.findItem(R.id.removeContactMi).setOnMenuItemClickListener {
+                        onContactClickListener.onRemoveContactMenuItemClick(adapterPosition)
+                        true
+                    }
+                }
+            }
+        }
     }
 
     // este método exige 3 métodos
