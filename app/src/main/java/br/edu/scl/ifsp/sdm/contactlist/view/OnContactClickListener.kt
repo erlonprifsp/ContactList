33a6d581b226526509cf7016498c4b7a29f434cf
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2023-11-24 09:41:48 -0300

    pratica - fase 3.2 - 1:55

--- app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/OnContactClickListener.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/contactlist/view/OnContactClickListener.kt
@@ -2,6 +2,13 @@ package br.edu.scl.ifsp.sdm.contactlist.view
 
 sealed interface OnContactClickListener {
 
-    //interface
+    //interface clique curto
     fun onContactClick(position: Int)
+
+    // interface clique longo - remover item
+    fun onRemoveContactMenuItemClick(position: Int)
+
+    // interface clique longo - editar item
+    fun onEditContactMenuItemClick(position: Int)
+
 }
